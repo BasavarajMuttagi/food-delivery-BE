9d2facbd233fac13b97e703626df792495cfa3ab
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-04-13 18:08:43 +0530

    add auth router

--- index.ts
+++ index.ts
@@ -5,11 +5,10 @@ import cors from "cors";
 config();
 
 import { createServer } from "http";
-// import { AuthRouter } from "./src/routes/auth.route";
 
-import { tokenType, validateToken } from "./src/middlewares/auth.middleware";
 import { MenuRouter } from "./src/routes/menu.route";
 import { OrderRouter } from "./src/routes/order.route";
+import { AuthRouter } from "./src/routes/auth.route";
 const App = express();
 const HttpServer = createServer(App);
 
@@ -18,8 +17,7 @@ export const DB_SECRET = process.env.DB_SECRET as string;
 
 App.use(cors());
 App.use(bodyParser.json());
-// App.use("/auth", AuthRouter);
-// App.use("/favorite", FavoriteRouter);
+App.use("/auth", AuthRouter);
 
 App.get("/", (req, res) => {
   return res.send("Hello World");
